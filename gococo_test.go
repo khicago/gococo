cb57ff23ef1847bcfec44f95f8e6627dd22f5546
bagaking <zfaustk@qq.com>
2020-11-15 02:04:19 +0800

    test: add more test cases, including the validation of incorrect situationn

--- gococo_test.go
+++ gococo_test.go
@@ -42,35 +42,66 @@ func TestParam_IsNil(t *testing.T) {
 }
 
 func TestParse(t *testing.T) {
-	strs := []string{
+	strs := []interface{}{
 		"<% Test %>",
 		"<% Num 3 4 5 _ 8 %>",
 		"<% Arguments _,arg1,arg2,arg3 %>",
 		"<% AllName alice,bob,cat,dada %>",
+		"<%YOUR_CMD:_,PARAM1,PARAM2,PARAM3%>",
+		"<% YOUR_CMD _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR_CMD %>",
+		"<% YOUR_CMD _ PARAM1 PARAM2 PARAM3 %>",
+		"<%    YOUR_CMD    :  _  ,  PARAM1  ,  PARAM2  ,   PARAM3   %>",
+		"<% YOUR---CMD: _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR.@#*&^(){}<>./\\CMD: _, PARAM.@#*&^(){}<>./\\ %>",
+		"<% YOUR一あ♂★CMD: _, PARAM.1, 一二三, あいう, 【】,♂★%>",
 	}
 	validate := []string{
 		"<% Test %>",
 		"<% Num: 3, 4, 5, _, 8 %>",
 		"<% Arguments: _, arg1, arg2, arg3 %>",
 		"<% AllName: alice, bob, cat, dada %>",
+		"<% YOUR_CMD: _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR_CMD: _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR_CMD %>",
+		"<% YOUR_CMD: _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR_CMD: _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR---CMD: _, PARAM1, PARAM2, PARAM3 %>",
+		"<% YOUR.@#*&^(){}<>./\\CMD: _, PARAM.@#*&^(){}<>./\\ %>",
+		"<% YOUR一あ♂★CMD: _, PARAM.1, 一二三, あいう, 【】, ♂★ %>",
 	}
-	ret, ok := gococo.Parse(fmt.Sprintf(`
-	this is a document
-	for %s
-	has %s
-	and %s
-	and %s
-	//`, strs[0], strs[1], strs[2], strs[3]))
+	testStr := fmt.Sprintf(`this is a document for %s has %s and %s
+	and %s and %s and %s and %s and %s and %s and %s and %s and %s and %s and strange symbols <> !@#$^&*()~
+	and %s and %s and %s and %s and %s and %s and %s and %s and %s and %s
+	//`, strs ...)
+	ret, ok := gococo.Parse(testStr)
 
 	if !ok {
 		t.Error("parse failed")
 	}
-
 	if len(ret) != len(strs) {
 		t.Fatalf("count of result error, expect= %d, got= %d", len(strs), len(ret))
 	}
 
 	for i, v := range ret {
 		matchOrError(validate[i], v.String(), fmt.Sprintf("parse error, ind= %d", i), t)
+	}
+}
+
+func TestParseError(t *testing.T) {
+	strs := []interface{}{
+		"<% YOUR_CMD: _, PARAM1, PARAM2, PARAM3, %>",
+		"<% YOUR_CMD: _, PAR~AM1, PARAM2, PARAM3, %>",
+		"<% YOUR~CMD: _, PARAM1, PARAM2, PARAM3, %>",
+		"<% YOUR_CMD: _, PARAM1, PARAM2, ,PARAM3, %>",
+		`<% YOUR_CMD: _, PARAM1, 
+		PARAM2, ,PARAM3, %>`,
+	}
+	for i, v := range strs {
+		testStr := fmt.Sprintf(`//this is a document for %s //`, v)
+		_, ok := gococo.Parse(testStr)
+		if ok {
+			t.Errorf("the %d case= %s should failed", i, v)
+		}
 	}
 }
